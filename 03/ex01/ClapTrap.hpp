4d2eb7699de4fbc0e6fee1355a146dc8d936fb3c
Jaewoo Park <ohdiar@gmail.com>
2022-07-14 17:38:46 +0900

    Prepared CPP03 ex01

---
+++ 03/ex01/ClapTrap.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaewpark <jaewpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 11:03:48 by jaewpark          #+#    #+#             */
+/*   Updated: 2022/07/14 17:16:58 by jaewpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# define COUT std::cout
+# define CLRRED "\e[91m"
+# define CLRGRE "\e[92m"
+# define CLRYEL "\e[93m"
+# define CLREND "\e[0m"
+
+# include <iostream>
+# include <iomanip>
+
+class ClapTrap
+{
+    public:
+        // Orthodox canonical class form
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap& clone);
+        ~ClapTrap();
+        ClapTrap& operator=(const ClapTrap& clone);
+
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+
+        void addEnergy(void);
+        void getStatus(void) const;
+        unsigned int getHp(void) const;
+        unsigned int getEnergy(void) const;
+        unsigned int getDamage(void) const;
+        void setHp(unsigned int const amout);
+        void setEnergy(unsigned int const amout);
+        void setDamage(unsigned int const amout);
+    
+        static unsigned int getMaxHp(void);
+    private:
+        std::string _name;
+        unsigned int _hp;
+        unsigned int _energy;
+        unsigned int _damage;
+        unsigned int _no_energy;
+
+        static unsigned int _max_hp;
+        static unsigned int _max_energy;
+};
+
+#endif
---
+++ 03/ex01/ScavTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaewpark <jaewpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 17:11:27 by jaewpark          #+#    #+#             */
+/*   Updated: 2022/07/14 17:16:15 by jaewpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(const ScavTrap& clone);
+        ~ScavTrap();
+        ScavTrap& operator=(const ScavTrap& clone);
+
+        void guardGate();
+    private:
+};
+
+#endif
