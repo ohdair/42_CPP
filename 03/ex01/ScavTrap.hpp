12eeb091cfc463be490b51ea9361f2eec6167a84
Jaewoo Park <ohdiar@gmail.com>
2022-07-15 14:35:01 +0900

    Modified Set Info

--- 03/ex01/ScavTrap.hpp
+++ 03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jaewpark <jaewpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 17:11:27 by jaewpark          #+#    #+#             */
-/*   Updated: 2022/07/15 14:21:50 by jaewpark         ###   ########.fr       */
+/*   Updated: 2022/07/15 14:34:47 by jaewpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ class ScavTrap: public ClapTrap
         void attack(const std::string& target);
         void guardGate(void);
     private:
-        static unsigned int _max_energy;
         static unsigned int _max_hp;
+        static unsigned int _max_energy;
         static unsigned int _default_damage;
 };
 
