fcb0e5b70c1dce799b50a3fb68c3fdbbb79fb54f
Jaewoo Park <ohdiar@gmail.com>
2022-08-01 20:27:27 +0900

    CPP 08 🐬

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jaewpark <jaewpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 11:19:24 by jaewpark          #+#    #+#             */
-/*   Updated: 2022/07/29 11:39:23 by jaewpark         ###   ########.fr       */
+/*   Updated: 2022/08/01 17:16:11 by jaewpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ typename T::iterator easyfind(T& first, const int second)
     typename T::iterator ret;
     try {
         ret = std::find(first.begin(), first.end(), second);
-        if (ret == std::end(first))
+        if (ret == first.end())
             throw (std::out_of_range("\e[91mNot found\e[0m ("));
         return ret;
     } catch(std::exception &e) {
